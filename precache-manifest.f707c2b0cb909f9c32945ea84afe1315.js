dcd5a18210901214cf3a9bdb0f1c606bdd2b5ea7
khan204 <alamgirkhan565@gmail.com>
2020-08-11 10:41:58 +0500

    Updates

---
+++ precache-manifest.f707c2b0cb909f9c32945ea84afe1315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445ee9975dcc0f23a8f38fe11a2cdd60",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "f139569ce866f182158d",
+    "url": "/react-cart/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "858f0c90e4253e660074",
+    "url": "/react-cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f139569ce866f182158d",
+    "url": "/react-cart/static/js/2.8f654b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cart/static/js/2.8f654b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858f0c90e4253e660074",
+    "url": "/react-cart/static/js/main.77e2795b.chunk.js"
+  },
+  {
+    "revision": "f5c2d20e17eadcb38ddf",
+    "url": "/react-cart/static/js/runtime-main.899e83dd.js"
+  }
+]);
